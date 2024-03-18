039775c601d9fae71eb639170d7ff3b04a91f8e5
klutz <820471452@qq.com>
2024-03-18 09:36:53 +0800

    finish lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,18 @@
 #include <string.h>
 #include "lfsr.h"
 
+uint16_t getx(uint16_t *reg){
+	uint16_t x=*reg&1;
+	x^=(*reg>>2)&1;
+	x^=(*reg>>3)&1;
+	x^=(*reg>>5)&1;
+	return x;
+}
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+		uint16_t head=getx(reg);
+		*reg=*reg>>1;
+		*reg=*reg|(head<<15);	
+
 }
 
